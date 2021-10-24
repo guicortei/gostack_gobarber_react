2f6c1229bf6f4190cd0a121b419cd28838059ebc
Guilherme Corrêa Teixeira <gui.cor.tei@gmail.com>
2021-10-24 15:04:21 -0300

    login page style

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
+import SignIn from './pages/SignIn';
+import GlobalStyle from './styles/global';
 
-function App() {
-  return <h1>Hello World</h1>;
-}
-
+const App: React.FC = () => (
+  <>
+    <SignIn />
+    <GlobalStyle />
+  </>
+);
 export default App;
